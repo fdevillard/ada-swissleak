f17b2e10b9b88eb9e2ea8673822f4742b9c274db
Semion Sidorenko <semion.sidorenko@edgecom.ch>
2016-11-30 15:13:39 +0100

    Add powernet scraper

--- zefix_scraper.py
+++ zefix_scraper.py
@@ -59,7 +59,6 @@ CHREGISTER_HEADERS = {
     "Host": "be.chregister.ch",
     #"Cache-Control": "max-age=0",
     "Faces-Request": "partial/ajax",
-    "Referer": "https://be.chregister.ch/cr-portal/auszug/auszug.xhtml?uid=CHE-110.398.897",
     "Accept": "application/xml, text/xml, */*; q=0.01",
     "Accept-Language": "en-US,en;q=0.8,fr;q=0.6,it;q=0.4",
     "Pragma": "no-cache",
@@ -72,6 +71,7 @@ CHREGISTER_HEADERS = {
 CHREGISTER_RES_URL = "https://be.chregister.ch/cr-portal/javax.faces.resource/bootstrap/css/bootstrap.min.css.xhtml;jsessionid={}?ln=default"
 
 #driver = webdriver.PhantomJS()
+
 
 def scrape_chregister(url):
     data = {
@@ -91,41 +91,79 @@ def scrape_chregister(url):
         # This kind of a CSRF token, needed to make AJAX requests
         viewstate = tree.xpath("//input[@id='j_id1:javax.faces.ViewState:0']/@value")[0]
 
-
         form_data = CHREGISTER_FORM.copy()
         form_data["javax.faces.ViewState"] = viewstate
+
+        headers = CHREGISTER_HEADERS.copy()
+        headers["Referer"] = url
+
         r = requests.post(
             "https://be.chregister.ch/cr-portal/auszug/auszug.xhtml;jsessionid={}".format(
                 JS1
             ),
             data=form_data,
-            headers=CHREGISTER_HEADERS,
+            headers=headers,
         ).content
 
         if "table class" in r.decode("utf-8"):
             partial_html = etree.fromstring(r).xpath("//update[@id='idPortalForm:auszugContentPanel']/text()")[0]
             partial_tree = html.fromstring("<html>"+partial_html+"</html>")
-
+            data["company_name"], data["id"] = partial_tree.xpath("//span[@class='firmaImportantInfo']/text()")
             for tr in partial_tree.xpath("//table[contains(@class, 'personen')]/tbody/tr"):
                 item = {}
                 attribs = tr[3].attrib
                 item["cancelled"] = "class" in attribs and "strike" in attribs["class"]
-                item_line = tr[3].xpath("./text()")[-1].split(",")
-                if len(item_line) >= 3:
-                    item["last_name"] = item_line[0].strip()
-                    item["first_name"] = item_line[1].strip()
+                split = tr[3].xpath("./text()")[-1].split(",")
+                if len(split) >= 3:
+                    item["last_name"] = split[0].strip()
+                    item["first_name"] = split[1].strip()
                     data["persons"].append(item)
                 else:
-                    item["name"] = item_line[0].strip()
+                    item["name"] = split[0].strip()
                     data["companies"].append(item)
             return data
 
     return None
 
+def scrape_powernet(url):
+    data = {
+        "persons": [],
+        "companies": [],
+    }
+
+    r = requests.get(url)
+
+    parser = html.HTMLParser(encoding="utf-8")
+    tree = html.fromstring(r.content, parser=parser)
+
+    data["id"] = tree.xpath("//table[3]//th[text()='Numéro de registre']/font/text()")
+    data["company_name"] = tree.xpath("//table[4]//table[1]//font[@color='#0000FF']/text()")
+
+    person_rows = tree.xpath("//table[descendant::th/text()='Indications personnelles']//tr/th[4]/font/descendant-or-self::text()")
+
+    for row in person_rows:
+        if not row.strip():
+            continue # Skip empty lines (no idea where they come from)
+
+        item = {}
+
+        #TODO factorize with chregister scraper
+        split = row.split(",")
+
+        if len(split) >= 3:
+            item["last_name"] = split[0].strip()
+            item["first_name"] = split[1].strip()
+            data["persons"].append(item)
+        else:
+            item["name"] = split[0].strip()
+            data["companies"].append(item)
+
+    return data
+
 
 SCRAPER_MAP = {
     "chregister.ch": scrape_chregister,
-    #"powernet.ch": scrape_powernet,
+    "powernet.ch": scrape_powernet,
     #"hrcintapp": scrape_hrcintapp,
 }
 
@@ -186,4 +224,10 @@ def scrape_company(name):
 
     # return data
 
-scrape_chregister("http://be.chregister.ch/cr-portal/auszug/auszug.xhtml?uid=CHE-110.398.897")
+POWERNET_TESTS = [
+"http://ti.powernet.ch/webservices/inet/HRG/HRG.asmx/getHRGHTML?chnr=5003002002&amt=501&toBeModified=0&validOnly=0&lang=2&sort=0",
+"http://ti.powernet.ch/webservices/inet/HRG/HRG.asmx/getHRGHTML?chnr=5146025097&amt=501&toBeModified=0&validOnly=0&lang=2&sort=0",
+]
+
+for t in POWERNET_TESTS:
+    print(scrape_powernet(t))
